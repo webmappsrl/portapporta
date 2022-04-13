6514e1487f293e67ff07a59a445592ddc470315b
bongiu <peppedeka@gmail.com>
2022-04-13 14:21:50 +0200

    add company

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Company::factory()->create(['name' => 'ersu']);
+        Company::factory()->create(['name' => 'asmiu']);
+        Company::factory()->create(['name' => 'rea']);
+        Company::factory()->create(['name' => 'esa']);
+        Company::factory()->create(['name' => 'sea']);
+    }
+}
