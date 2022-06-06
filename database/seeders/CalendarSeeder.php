9443739ecfaae7216a645040a79e3b8da765012f
Alessio Piccioli <piccioli@netseven.it>
2022-06-06 13:16:58 +0200

    app.pap.222.14.1: model Calendar created with seeder

---
+++ database/seeders/CalendarSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Calendar;
+use App\Models\Company;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CalendarSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $year = date('Y');
+        $winter_start=Carbon::parse("$year-01-01 00:00:00");
+        $winter_stop=Carbon::parse("$year-06-30 11:59:59");
+        $summer_start=Carbon::parse("$year-07-01 00:00:00");
+        $summer_stop=Carbon::parse("$year-12-31 11:59:59");
+        foreach(Company::all() as $company) {
+
+            // winter calendar 01/01/this year -> 30/06/this-year
+            $data_winter = [
+                'name' => "WINTER Calendar $year for Company {$company->name}",
+                'start_date' => $winter_start,
+                'stop_date' => $winter_stop,
+                'company_id' => $company->id
+            ];
+            Calendar::factory()->create($data_winter);
+            // summer calendar
+            $data_summer = [
+                'name' => "SUMMER Calendar $year for Company {$company->name}",
+                'start_date' => $summer_start,
+                'stop_date' => $summer_stop,
+                'company_id' => $company->id
+            ];
+            Calendar::factory()->create($data_summer);
+
+        }
+    }
+}
