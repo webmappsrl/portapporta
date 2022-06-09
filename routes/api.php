1899f94d35b38b7557ac13cb6386d601d6c78510
bongiu <peppedeka@gmail.com>
2022-06-09 17:42:25 +0200

    ...

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,6 @@ use App\Http\Resources\RifiutarioResource;
 use App\Http\Resources\TrashTypeResource;
 use App\Http\Resources\UtenzeMetaResource;
 use App\Http\Resources\ZoneConfiniResource;
-use App\Http\Resources\ZoneMetaResource;
 use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,48 +26,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout']);
-Route::prefix('c')->name('company.')->middleware('auth:sanctum', 'signed')->group(function () {
 
-    // Route::get('/{id}/info.json', function ($id) {
-    Route::get('/{id}/config.json', function ($id) {
-        return new CompanyResource(Company::findOrFail($id));
-    })->name('config.json');
-
-    // LEGACY Route::get('/{id}/data/utenze_meta.json', function ($id) {
-    Route::get('/{id}/user_types.json', function ($id) {
-        return new UtenzeMetaResource(Company::findOrFail($id));
-    })->name('user_types.json');
-
-    // LEGACY Route::get('/{id}/data/tipi_rifiuto.json', function ($id) {
-    Route::get('/{id}/trash_types.json', function ($id) {
-        return new TrashTypeResource(Company::findOrFail($id));
-    })->name('trash_types.json');
-
-    // LEGACY Route::get('/{id}/data/rifiutario.json', function ($id) {
+// NO MIDDLEWARES
+Route::prefix('c')->name('company.')->group(function () {
     Route::get('/{id}/wastes.json', function ($id) {
         return new RifiutarioResource(Company::findOrFail($id));
     })->name('wastes.json');
+    Route::get('/{id}/user_types.json', function ($id) {
+        return new UtenzeMetaResource(Company::findOrFail($id));
+    })->name('user_types.json');
+    Route::get('/{id}/trash_types.json', function ($id) {
+        return new TrashTypeResource(Company::findOrFail($id));
+    })->name('trash_types.json');
+    Route::get('/{id}/zones.geojson', function ($id) {
+        return new ZoneConfiniResource(Company::findOrFail($id));
+    })->name('zones.geojson');
+});
 
-    // LEGACY Route::get('/{id}/data/centri_raccolta.geojson', function ($id) {
+
+// AUTH
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// AUTH AND SIGNED
+Route::prefix('c')->name('company.')->middleware('auth:sanctum', 'signed')->group(function () {
+    Route::get('/{id}/config.json', function ($id) {
+        return new CompanyResource(Company::findOrFail($id));
+    })->name('config.json');
     Route::get('/{id}/waste_collection_centers.geojson', function ($id) {
         return new CentriRaccoltaResource(Company::findOrFail($id));
     })->name('waste_collection_centers.geojson');
-
-    Route::get('/{id}/data/zone_confini.geojson', function ($id) {
-        return new ZoneConfiniResource(Company::findOrFail($id));
-    })->name('zone_confini.geojson');
-
-    Route::get('/{id}/data/zone_meta.json', function ($id) {
-        return new ZoneMetaResource(Company::findOrFail($id));
-    })->name('zone_meta.json');
-
-    // Ticket
     Route::post('/{id}/ticket', [TicketController::class, 'store'])->name('ticket');
     Route::get('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
 });
