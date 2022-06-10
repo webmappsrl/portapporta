23a43a0c92b4d7365de0350c0e662fc2e81a0c21
Alessio Piccioli <piccioli@netseven.it>
2022-06-10 22:17:34 +0200

    app.pap.222.14.2: Resource calendar created and minimum setup

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Calendar extends Model
 {
     use HasFactory;
+    protected $casts = [
+        'start_date' => 'date:Y-m-d',
+        'stop_date' => 'date:Y-m-d',
+    ];
+        
 }
