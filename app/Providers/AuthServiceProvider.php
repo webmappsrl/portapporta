14a143d3a6bda408c3e7d5d6bd7c7088a482fc26
Pedram Katanchi <pedramkat@gmail.com>
2022-06-17 11:27:28 +0200

    app.pap.222.21.3

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Zone;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -26,11 +27,18 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            $zone = Zone::find($notifiable['zone_id'])->first();
+            $company = strtoupper($zone->company->name);
+            $user_name = $notifiable['name'];
             return (new MailMessage)
-                ->from('noreply@webmapp.it', 'portAPPorta')
-                ->subject('Verifica email portAPPorta')
-                ->line('Fai click sul pulsante qui sotto per verificare il tuo indirizzo email.')
-                ->action('verifica indirizzo e-mail', $url);
+                ->from('noreply@webmapp.it', "PortAPPorta-$company")
+                ->subject("Conferma email - APP PortAPPorta-$company")
+                ->greeting("Salve, $user_name")
+                ->line("Riceve questa email perché è stata effettuata una registrazione sulla APP PortAPPorta-$company con il suo indirizzo email")
+                ->line("Per completare le registrazione faccia click sul pulsante qui sotto che permette di verificare il suo indirizzo email")
+                ->action('verifica indirizzo email', $url)
+                ->line("Se non ha effettuato lei la registrazione, ignori questa email")
+                ->salutation("Grazie, il TEAM di PortAPPorta-$company");
         });
         //
     }
