9852e488877e066326c82f7f3ccc96bd20de6b28
Alessio Piccioli <piccioli@netseven.it>
2022-07-23 15:09:08 +0200

    New dashboard with user and tickets

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -2,11 +2,17 @@
 
 namespace App\Nova\Dashboards;
 
-use Laravel\Nova\Cards\Help;
+use App\Nova\Metrics\NewTicketsValueMetric;
+use App\Nova\Metrics\NewUsersValueMetric;
 use Laravel\Nova\Dashboards\Main as Dashboard;
 
 class Main extends Dashboard
 {
+
+    public function name() {
+        return 'PortAPPorta';
+    }
+
     /**
      * Get the cards for the dashboard.
      *
@@ -15,7 +21,8 @@ class Main extends Dashboard
     public function cards()
     {
         return [
-            new Help,
+            new NewUsersValueMetric,
+            new NewTicketsValueMetric,
         ];
     }
 }
