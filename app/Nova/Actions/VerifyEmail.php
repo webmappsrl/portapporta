967395b36eba401ec816424e01201ae491d27c8e
peppedeka <peppedeka@gmail.com>
2024-02-12 15:15:36 +0100

    feat: add VerifyEmail action in Nova
    A new action named VerifyEmail has been added to the Nova actions. This action checks if an email is verified, and if not, it sets the 'email_verified_at' field to the current time. The action returns a success message upon successful verification of emails.

---
+++ app/Nova/Actions/VerifyEmail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class VerifyEmail extends Action
+{
+    use InteractsWithQueue, Queueable;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        foreach ($models as $model) {
+            if (is_null($model->email_verified_at)) {
+                $model->email_verified_at = now();
+                $model->save();
+            }
+        }
+
+        return Action::message('Email verified successfully!');
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [];
+    }
+}
