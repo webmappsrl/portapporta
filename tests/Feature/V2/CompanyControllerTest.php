8561b0119cf989e800b8436b955aecb1cece551f
FalconClaws059 <83666067+FalconClaws059@users.noreply.github.com>
2024-12-17 08:21:57 +0100

    chore: Added a series of test for CompanyController oc:4472 (#31)
    * chore: Added a series of test for CompanyController oc:4472
    
    * chore: added test for form_json fields and passed it as an array oc:4472

---
+++ tests/Feature/V2/CompanyControllerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\Company;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+class CompanyControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+    private $company;
+    private $anotherCompany;
+    private const API_PREFIX = '/api/v2/c/';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->company = Company::factory()->create(
+            [
+                'form_json' => json_encode(['name' => 'John', 'age' => 30])
+            ]
+        );
+        $this->anotherCompany = Company::factory()->create();
+    }
+
+    public function testFormJson()  
+    {
+        $this->get(self::API_PREFIX . $this->company->id . '/form_json')
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->where('success', true)
+                ->has('data', fn (AssertableJson $json) =>
+                    $json->where('name', 'John')
+                         ->where('age', 30)
+                )
+                ->where('message', 'Json of the registration form fields.')
+        );
+    }
+
+    public function testFormJsonWithInvalidId()
+    {
+        $response = $this->get(self::API_PREFIX . '0/form_json');
+        $response->assertStatus(500);
+    }
+
+    public function testFormJsonWithEmptyFormJson()
+    {
+        $response = $this->get(self::API_PREFIX . $this->anotherCompany->id . '/form_json');
+        $response->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->where('success', true)
+                ->where('data', null)
+                ->where('message', 'Json of the registration form fields.')
+        );
+    }
+}
