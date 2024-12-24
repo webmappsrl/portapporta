ccf74af7bf5cd9bdfc6babf5531f281ec39c0f8b
FalconClaws059 <83666067+FalconClaws059@users.noreply.github.com>
2024-12-24 09:20:39 +0100

    chore: Added a test to check the Populate form data function in User Model oc:4526 (#40)
    * chore: Added a test to check the Populate form data function oc:4526
    
    * chore: added new factory function for form_data oc:4526

---
+++ tests/Unit/ModelUserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ModelUserTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * Test that populateFormData correctly populates the form_data attribute.
+     *
+     * @return void
+     */
+    public function testPopulateFormData()
+    {
+        $expectedFormData = [
+            'phone_number' => '123-456-7890',
+            'fiscal_code'  => 'ABCDEF123456',
+            'user_code'    => 'USER123',
+        ];
+        $user = User::factory()->withoutFormData()->create([
+            ...$expectedFormData,
+        ]);
+        $user->populateFormData();
+        $user->refresh();
+        $this->assertEquals($expectedFormData, $user->form_data);
+        $this->assertFalse($user->timestamps);
+    }
+
+}
